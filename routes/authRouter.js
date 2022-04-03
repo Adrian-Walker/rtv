bbdc8d8e344a25c2633c1a45a8c05d6c97c2d68c
Adrian Walker <awalker3365@gmail.com>
2022-04-03 14:17:17 -0400

    commentRouter added

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -30,29 +30,29 @@ authRouter.post("/signup", (req, res, next) => {
 // Login
 authRouter.post("/login", (req, res, next) => {
     User.findOne({ username: req.body.username.toLowerCase() }, (err, user) => {
-      if(err){
-        res.status(500)
-        return next(err)
-      }
-      if(!user){
-        res.status(403)
-        return next(new Error("Username or Password are incorrect"))
-      }
+        if (err) {
+            res.status(500)
+            return next(err)
+        }
+        if (!user) {
+            res.status(403)
+            return next(new Error("Username or Password are incorrect"))
+        }
 
-      user.checkPassword(req.body.password, (err, isMatch) => {
-        if(err) {
-          res.status(403)
-          return next(new Error("Username or Password are incorrect"))
-        }
-        if(!isMatch){
-          res.status(403)
-          return next(new Error(`Username or Password are incorrect`))
-        }
-        const token = jwt.sign(user.withoutPassword(), process.env.SECRET)
-        return res.status(200).send({ token, user: user.withoutPassword() })
-      })
+        user.checkPassword(req.body.password, (err, isMatch) => {
+            if (err) {
+                res.status(403)
+                return next(new Error("Username or Password are incorrect"))
+            }
+            if (!isMatch) {
+                res.status(403)
+                return next(new Error(`Username or Password are incorrect`))
+            }
+            const token = jwt.sign(user.withoutPassword(), process.env.SECRET)
+            return res.status(200).send({ token, user: user.withoutPassword() })
+        })
     })
-  })
+})
 
 
-  module.exports = authRouter
+module.exports = authRouter
